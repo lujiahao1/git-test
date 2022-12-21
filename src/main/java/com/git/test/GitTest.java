c7b6e296e5c82ecac51a590000fd51733a0008bd
lujiahao <1992332536@qq.com>
2022-12-21 20:51:40 +0800

    second commit

--- src/main/java/com/git/test/GitTest.java
+++ src/main/java/com/git/test/GitTest.java
@@ -3,5 +3,6 @@ package com.git.test;
 public class GitTest {
     public static void main(String[] args) {
         System.out.println("hello Git!");
+        System.out.println("hello Git!");
     }
 }
